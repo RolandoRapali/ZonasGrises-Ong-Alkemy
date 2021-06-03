dc2533716edf028a946020e393c3528f05fff153
Mauricio Pino <mauripino1998cat@gmail.com>
2021-06-03 17:28:48 -0300

    Test de Commit en Branch

--- src/main/java/alkemy/challenge/Challenge/Alkemy/ChallengeSpringApplication.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/ChallengeSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChallengeSpringApplication {
 
+	//Test de Commit
 
 
 	public static void main(String[] args) {
