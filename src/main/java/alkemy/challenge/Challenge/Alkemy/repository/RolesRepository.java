f0d0d2f8066ae61d8d3e0cbd77fd0450b0ea8671
maximiliano <maxigiuliano18@gmail.com>
2021-06-03 18:13:52 -0300

    "Agregue controller, Modelo y Repositorio (Roles)"

---
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/repository/RolesRepository.java
@@ -0,0 +1,11 @@
+package alkemy.challenge.Challenge.Alkemy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import alkemy.challenge.Challenge.Alkemy.model.Roles;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Roles, Long> {
+    
+}
