9dc9623ad7d358c994624cb5625aedad8ce974be
Desarrollo <Desarrollo@DESKTOP-61GMHDM>
2021-06-04 15:37:10 -0400

    Cambio de nombre: "IMembersRepository" a "MembersRepository"

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MembersController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MembersController.java
@@ -2,12 +2,12 @@ package alkemy.challenge.Challenge.Alkemy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import alkemy.challenge.Challenge.Alkemy.repository.IMembersRepository;
+import alkemy.challenge.Challenge.Alkemy.repository.MembersRepository;
 
 @Controller
 public class MembersController {
 
     @Autowired
-    private IMembersRepository membersRepository;
+    private MembersRepository membersRepository;
 
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/repository/MembersRepository.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/repository/MembersRepository.java
@@ -3,6 +3,6 @@ package alkemy.challenge.Challenge.Alkemy.repository;
 import alkemy.challenge.Challenge.Alkemy.model.Members;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IMembersRepository extends CrudRepository<Members, Long> {
+public interface MembersRepository extends CrudRepository<Members, Long> {
 
 }
