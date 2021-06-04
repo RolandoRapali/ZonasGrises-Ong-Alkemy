0dc586ba5768d9649f97c40a1188b68dc1c3dd14
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2021-06-04 00:17:54 -0300

    model (entity), repository, service y controller

---
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/model/Organization.java
@@ -0,0 +1,40 @@
+package alkemy.challenge.Challenge.Alkemy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "organization")
+public class Organization implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String image;
+
+    private String address;
+
+    private Integer phone;
+    @NotNull
+    private String email;
+    @NotNull
+    private String welcomeText;
+
+    private String aboutUsText;
+
+    private boolean isDeleted = false;
+
+    private Timestamp timestamp;
+}
---
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package alkemy.challenge.Challenge.Alkemy.repository;
+
+import alkemy.challenge.Challenge.Alkemy.model.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
