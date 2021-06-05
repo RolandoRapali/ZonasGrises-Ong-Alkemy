c70039ac5c9b259134ded121b5134ba29f5c0ba7
maximiliano <maxigiuliano18@gmail.com>
2021-06-04 22:03:01 -0300

    Correcion de inyeccion autowired en la carpeta correspondiente

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/CategoriesController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/CategoriesController.java
@@ -1,29 +1,22 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import alkemy.challenge.Challenge.Alkemy.model.Categories;
-import alkemy.challenge.Challenge.Alkemy.repository.CategoriesRepository;
 import alkemy.challenge.Challenge.Alkemy.service.CategoriesService;
 
 @RestController
 public class CategoriesController {
     @Autowired
-    private CategoriesRepository categoriesRepository;
-
-    @Autowired
     private CategoriesService categoriesService;
 
     @GetMapping("/categories")
     public String listCategoriesByName(Model model){
-        List<Categories> listCategories = categoriesRepository.findAll();
         /*Hago impresion de la lista con las categorias por nombre*/
-        System.out.println("List of Categories: "+ categoriesService.listCategoriesByName(listCategories));
+        System.out.println("List of Categories: "+ categoriesService.listCategoriesByName());
         /*Mando la lista a la vista*/
-        model.addAttribute("listCategories", categoriesService.listCategoriesByName(listCategories));
+        model.addAttribute("listCategories", categoriesService.listCategoriesByName());
         return "categoriesView";
     }
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/CategoriesService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/CategoriesService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import alkemy.challenge.Challenge.Alkemy.model.Categories;
-
+import alkemy.challenge.Challenge.Alkemy.repository.CategoriesRepository;
 @Service
 public class CategoriesService {
+    @Autowired
+    private CategoriesRepository categoriesRepository;
 
     /*Obtener lista de categorias por nombres*/
-    public List<String> listCategoriesByName(List<Categories> listCategories){
+    public List<String> listCategoriesByName(){
+        List<Categories> listCategories = categoriesRepository.findAll();
         List<String> listCategoriesByname = new ArrayList<>();
         for (Categories c : listCategories) {
             listCategoriesByname.add(c.names);
