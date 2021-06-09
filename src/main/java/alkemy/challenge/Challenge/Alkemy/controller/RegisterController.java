4a2af79e58cfed847edabc9141b6abf2221a99a3
Mauricio Pino <mauripino1998cat@gmail.com>
2021-06-09 12:38:59 -0300

    * Se agregó el método para obtener la información del usuario logeado.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/RegisterController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/RegisterController.java
@@ -2,16 +2,21 @@ package alkemy.challenge.Challenge.Alkemy.controller;
 
 import alkemy.challenge.Challenge.Alkemy.model.User;
 import alkemy.challenge.Challenge.Alkemy.exception.UserAlreadyExistException;
+import alkemy.challenge.Challenge.Alkemy.security.authentication.AuthenticationRequest;
+import alkemy.challenge.Challenge.Alkemy.security.authentication.AuthenticationResponse;
 import alkemy.challenge.Challenge.Alkemy.service.ErrorHandlingService;
 import alkemy.challenge.Challenge.Alkemy.service.UserService;
+import alkemy.challenge.Challenge.Alkemy.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +30,15 @@ public class RegisterController {
 
     @Autowired
     private ErrorHandlingService errorHandlingService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @Autowired
+    private UserService userDetailsService;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) throws UserAlreadyExistException {
@@ -42,5 +56,24 @@ public class RegisterController {
 
         response.put("user", user);
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/repository/UserRepository.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/repository/UserRepository.java
@@ -8,12 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    //Soft Delete Query's
-//    @Query("UPDATE FROM users SET deleted = 1 WHERE id = ?1")
-//    public void deleteUser(Long id);
-//
-//    @Query("SELECT * FROM users WHERE deleted = 0")
-//    public List<User> usersWithStatusActive();
     Optional<User> findByEmail(String email);
 
 }
