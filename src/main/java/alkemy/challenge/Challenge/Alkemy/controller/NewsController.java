88cd660bc01371ac7a6cdcf415ee1be9dc13aa93
Desarrollo <Desarrollo@DESKTOP-61GMHDM>
2021-06-09 15:16:50 -0400

    Se creo en NewsController el metodo DELETE /news/:id para eliminar id, en NewsService se implementa la validacion del borrado por id, si existe se elimina, tambien se agrega public void deleteById(long id); a NewsRepository.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/NewsController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/NewsController.java
@@ -5,6 +5,7 @@ import alkemy.challenge.Challenge.Alkemy.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,13 @@ public class NewsController {
     public News bringNews(@PathVariable long id) {
         News news = newsService.bringById(id);
         return news;
+    }
+
+    @DeleteMapping("/news/{id}")
+    public String delete(@PathVariable("id") long id) {
+        newsService.delete(id);
+        return "redirect:/news";
+
     }
 
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/NewsService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/NewsService.java
@@ -2,6 +2,7 @@ package alkemy.challenge.Challenge.Alkemy.service;
 
 import alkemy.challenge.Challenge.Alkemy.model.News;
 import alkemy.challenge.Challenge.Alkemy.repository.NewsRepository;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +16,13 @@ public class NewsService {
         return newsRepository.findById(id);
     }
 
+    @Transactional
+    public void delete(long id) {
+        boolean exists = newsRepository.existsById(id);
+        if (exists) {
+            newsRepository.deleteById(id);
+        } else {
+            throw new IllegalStateException("El id " + id + "que buscas no existe");
+        }
+    }
 }
