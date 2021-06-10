dc0e2863dc8d949f2e208d4ad7d770826fb86f7d
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2021-06-10 16:14:54 -0300

    fix, consultado con Saúl, cambio interface por class

--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlidesService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlidesService.java
@@ -1,18 +1,13 @@
 package alkemy.challenge.Challenge.Alkemy.service;
 
-import alkemy.challenge.Challenge.Alkemy.model.Slides;
-import java.util.List;
+import alkemy.challenge.Challenge.Alkemy.repository.OrganizationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface SlidesService {
+@RequiredArgsConstructor
+public class SlidesService {
 
-    public List<Slides> listSlides();
-
-    public void saveSlides(Slides slides);
-
-    public void deleteSlides(Slides slides);
-
-    public Slides findSlides(Slides slides);
+    private final OrganizationRepository organizationRepository;
 
 }
