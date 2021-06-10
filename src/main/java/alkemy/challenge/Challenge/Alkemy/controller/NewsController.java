5d24fc48cba5d14e3fc40e7cd6675cb726fbbc57
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2021-06-10 20:20:15 -0300

    update news

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/NewsController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/NewsController.java
@@ -3,11 +3,13 @@ package alkemy.challenge.Challenge.Alkemy.controller;
 import alkemy.challenge.Challenge.Alkemy.model.News;
 import alkemy.challenge.Challenge.Alkemy.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/news")
@@ -21,6 +23,17 @@ public class NewsController {
     public News bringNews(@PathVariable long id) {
         News news = newsService.bringById(id);
         return news;
+    }
+
+    @PreAuthorize("hasRole(ROLE_ADMIN)")
+    @PutMapping("/{id}")
+    public ResponseEntity<News> updateNews(@PathVariable Long id, @RequestBody @Valid News news) {
+        News newsAux = newsService.bringById(id);
+        if (!(newsAux == null)) {
+            newsService.update(newsAux, news);
+            return ResponseEntity.ok(newsAux);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 
 }
