2d85248abe9f5435e8219eba97818fc97976fafd
RolandoRapali <RolandoRapali@users.noreply.github.com>
2021-06-11 13:07:45 -0300

    Se agregaron las validaciones

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/OrganizationController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/OrganizationController.java
@@ -3,12 +3,17 @@ package alkemy.challenge.Challenge.Alkemy.controller;
 import alkemy.challenge.Challenge.Alkemy.dto.OrganizationDto;
 import alkemy.challenge.Challenge.Alkemy.dto.OrganizationDtoConverter;
 import alkemy.challenge.Challenge.Alkemy.service.OrganizationService;
+import alkemy.challenge.Challenge.Alkemy.util.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/organization")
@@ -25,8 +30,19 @@ public class OrganizationController {
     }
 
     @PostMapping("/public")
-    public void modifyOrganization(@RequestBody OrganizationDto organizationDto) {
-        organizationService.update(organizationDto);
+    public ResponseEntity<?> modifyOrganization(@RequestBody @Valid OrganizationDto organizationDto) {
+        if (organizationDto.getName().isEmpty()) {
+            return new ResponseEntity(new Message("el campo de nombre no puede estar vacio"), HttpStatus.BAD_REQUEST);
+        }
+        if (organizationDto.getImage().isEmpty()) {
+            return new ResponseEntity(new Message("el campo de imagen no puede estar vacio"), HttpStatus.BAD_REQUEST);
+        } else {
+            organizationService.update(organizationDto);
+            return new ResponseEntity(new Message("la organizacion ha sido modificada con exito."), HttpStatus.OK);
+
+        }
+
+
     }
 
 
