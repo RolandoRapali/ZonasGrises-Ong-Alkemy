4cec9eeb078ed0748014cd8271e23ac8541f0cd9
RolandoRapali <RolandoRapali@users.noreply.github.com>
2021-06-11 12:53:21 -0300

    Se creo los metodos necesarios para modificar la organizacion

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/OrganizationController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/OrganizationController.java
@@ -1,8 +1,13 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
+import alkemy.challenge.Challenge.Alkemy.dto.OrganizationDto;
+import alkemy.challenge.Challenge.Alkemy.dto.OrganizationDtoConverter;
 import alkemy.challenge.Challenge.Alkemy.service.OrganizationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrganizationController {
 
     private final OrganizationService organizationService;
+
+    private final OrganizationDtoConverter organizationDtoConverter;
+
+    @GetMapping("/public")
+    public OrganizationDto bringOrganization(@RequestParam(name = "id", defaultValue = "1") int id) {
+        return organizationDtoConverter.convertEntityToGetOrganizationDto(organizationService.bringOrganization(id));
+    }
+
+    @PostMapping("/public")
+    public void modifyOrganization(@RequestBody OrganizationDto organizationDto) {
+        organizationService.update(organizationDto);
+    }
+
+
 }
