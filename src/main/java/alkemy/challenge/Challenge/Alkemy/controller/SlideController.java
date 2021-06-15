6ffc864a4395d8d96c2f2f4ba7460b22f174f939
maximiliano <maxigiuliano18@gmail.com>
2021-06-15 18:35:13 -0300

    detalle de slide, endpoint para mostrar el detalle del slide

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
@@ -1,7 +1,10 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
+import alkemy.challenge.Challenge.Alkemy.model.Slide;
 import alkemy.challenge.Challenge.Alkemy.service.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,9 @@ public class SlideController {
 
     @Autowired
     private SlideService slideService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Slide> detailSlide(@PathVariable(value = "id") Long id){
+        return slideService.detailSlide(id);
+    }
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
@@ -2,12 +2,18 @@ package alkemy.challenge.Challenge.Alkemy.service;
 
 import alkemy.challenge.Challenge.Alkemy.repository.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SlideService {
 
-    private final OrganizationRepository organizationRepository;
-
+    public ResponseEntity<Slide> detailSlide(Long id){
+        if(slideRepository.existsById(id)){
+            Slide s = slideRepository.getById(id);
+            return ResponseEntity.ok(s);
+        }
+        return (ResponseEntity<Slide>) ResponseEntity.notFound();
+    }
 }
