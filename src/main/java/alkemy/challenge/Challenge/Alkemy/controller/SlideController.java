41f5ecff64b4a715bf278f647bdd998cce7f1ec7
maximiliano <maxigiuliano18@gmail.com>
2021-06-15 18:13:47 -0300

    metodo para obtener listado de slides, peticion get en controller para mostrar listado

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
@@ -1,7 +1,10 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
 import alkemy.challenge.Challenge.Alkemy.service.SlideService;
+
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,10 @@ public class SlideController {
 
     @Autowired
     private SlideService slideService;
+
+    /*listado de slides con la imagen y el orden del mismo*/
+    @GetMapping("/")
+    public List<String> listSlides(){
+        return slideService.listSlides();
+    }
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
@@ -1,13 +1,27 @@
 package alkemy.challenge.Challenge.Alkemy.service;
 
+import alkemy.challenge.Challenge.Alkemy.model.Slide;
 import alkemy.challenge.Challenge.Alkemy.repository.OrganizationRepository;
+import alkemy.challenge.Challenge.Alkemy.repository.SlideRepository;
 import lombok.RequiredArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SlideService {
+    @Autowired
+    private SlideRepository slideRepository;
 
-    private final OrganizationRepository organizationRepository;
-
+    public List<String> listSlides(){
+        List<Slide> listSlide = slideRepository.findAll();
+        List<String> listImageSequence = new ArrayList<>();
+        for (Slide s : listSlide) {
+            listImageSequence.add(s.getImageUrl());
+            listImageSequence.add(s.getSequence());            
+        }
+        return listImageSequence;
+    }
 }
