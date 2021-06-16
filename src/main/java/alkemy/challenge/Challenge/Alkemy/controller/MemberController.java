157bd63d4da9ea1bc04f9c950620c50ff5b48bdb
Mauricio Pino <mauripino1998cat@gmail.com>
2021-06-16 14:59:30 -0300

    * Se agregó el método GET para obtener la lista de todos los miembros registrados.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MemberController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MemberController.java
@@ -1,9 +1,13 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
+import alkemy.challenge.Challenge.Alkemy.model.Member;
 import alkemy.challenge.Challenge.Alkemy.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
@@ -11,5 +15,10 @@ public class MemberController {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @GetMapping
+    public List<Member> listMembers(){
+        return (List<Member>) memberRepository.findAll();
+    }
 
 }
