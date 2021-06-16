e9f083b96bda85dc90cd53c466b112c6394a6900
maximiliano <maxigiuliano18@gmail.com>
2021-06-15 21:54:27 -0300

    actualizar slide

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
@@ -2,6 +2,7 @@ package alkemy.challenge.Challenge.Alkemy.controller;
 
 import alkemy.challenge.Challenge.Alkemy.service.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,10 @@ public class SlideController {
 
     @Autowired
     private SlideService slideService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Slide> updateSlide(@PathVariable(value = "id")Long id, 
+            @Valid @RequestBody Slide slideDetails){
+        return slideService.updateSlide(id, slideDetail);
+    }
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
@@ -1,6 +1,4 @@
 package alkemy.challenge.Challenge.Alkemy.service;
-
-import alkemy.challenge.Challenge.Alkemy.repository.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +6,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SlideService {
 
-    private final OrganizationRepository organizationRepository;
-
+    public ResponseEntity<Slide> updateSlide(Long id, Slide slideDetail){
+        if (slideRepository.existsById(id)){
+            Slide slide = slideRepository.getById(id);
+            slide.setImageUrl(slideDetail.getImageUrl());
+            slide.setText(slideDetail.getText());
+            slide.setSequence(slideDetail.getSequence());
+            slide.setCreatedAt(slideDetail.getCreatedAt());
+            slide.setUpdatedAt(slideDetail.getUpdatedAt());
+            final Slide updatSlide = slideRepository.save(slide);
+            return ResponseEntity.ok(updatSlide);
+        }
+        return (ResponseEntity<Slide>) ResponseEntity.notFound();
+    }
 }
