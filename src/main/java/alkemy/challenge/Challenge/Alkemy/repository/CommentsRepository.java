5de9e953328fe5be3afcee5cc6412750eaf4b05f
Desarrollo <Desarrollo@DESKTOP-61GMHDM>
2021-06-16 17:48:01 -0400

    Se creo CommentsController y metodo que devuelve comentarios pertenecientes a News en CommentsController, CommentsService y CommentsRepository.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/repository/CommentsRepository.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/repository/CommentsRepository.java
@@ -1,10 +1,13 @@
 package alkemy.challenge.Challenge.Alkemy.repository;
 
 import alkemy.challenge.Challenge.Alkemy.model.Comments;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
-    
+
+    public List<Comments> findByNewsId(Long id);
+
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/CommentsService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/CommentsService.java
@@ -2,14 +2,19 @@ package alkemy.challenge.Challenge.Alkemy.service;
 
 import alkemy.challenge.Challenge.Alkemy.model.Comments;
 import alkemy.challenge.Challenge.Alkemy.repository.CommentsRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CommentsService {
-    
+
     @Autowired
     private CommentsRepository commentsRepository;
-    
+
     public void save(Comments comment) {
         commentsRepository.save(comment);
+    }
+
+    public List<Comments> listComments(Long id) {
+        return commentsRepository.findByNewsId(id);
     }
 }
