59354a644dfaf378dfc236e3a303113a6ba25bed
Mauricio Pino <mauripino1998cat@gmail.com>
2021-06-16 15:16:21 -0300

    * Se agregó el método DELETE para eliminar miembros existentes. Si el ID del miembro es 0, lanzará un error, como también cuando este sea inexistente.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MemberController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MemberController.java
@@ -1,9 +1,14 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
+import alkemy.challenge.Challenge.Alkemy.model.Member;
 import alkemy.challenge.Challenge.Alkemy.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/members")
@@ -11,5 +16,27 @@ public class MemberController {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @GetMapping
+    public List<Member> listMembers(){
+        return (List<Member>) memberRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMember(@PathVariable(value = "id") Long id){
+        Map<String, Object> response = new HashMap<>();
+
+        if(id > 0){
+            Member member = memberRepository.findById(id).orElse(null);
+            if(member == null){
+                response.put("member_null","Member doesn't exist");
+            }
+        } else {
+            response.put("id_0","The ID cannot be 0");
+        }
+
+    }
+
+    
 
 }
