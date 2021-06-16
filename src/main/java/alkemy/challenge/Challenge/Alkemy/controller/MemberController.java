5de50fc611c3e8b0945fe2947fbccb2179ed9d52
Mauricio Pino <mauripino1998cat@gmail.com>
2021-06-16 15:42:38 -0300

    * Se agregó el método PUT para actualizar miembros existentes. Se validó que el ID sea mayor a 0 como que el ID sea válido, de otra forma se devolverá un error.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MemberController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/MemberController.java
@@ -1,9 +1,17 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
+import alkemy.challenge.Challenge.Alkemy.model.Member;
 import alkemy.challenge.Challenge.Alkemy.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/members")
@@ -11,5 +19,25 @@ public class MemberController {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editMember(@PathVariable(value = "id") Long id) {
+        Map<String, Object> response = new HashMap<>();
+
+        Member member = null;
+        if (id > 0) {
+            member = memberRepository.findById(id).orElse(null);
+            memberRepository.save(member);
+            if (member == null) {
+                response.put("member_doesnt_exist", "Member with id " + id + " doesn't exist");
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            response.put("id_0", "The ID cannot be 0");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.ACCEPTED);
+    }
 
 }
