0fbc2ec2f1c7884fbe69318887ca1088441450d1
maximiliano <maxigiuliano18@gmail.com>
2021-06-15 21:15:20 -0300

    borrar slide

--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/SlideService.java
@@ -1,13 +1,23 @@
 package alkemy.challenge.Challenge.Alkemy.service;
 
-import alkemy.challenge.Challenge.Alkemy.repository.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SlideService {
 
-    private final OrganizationRepository organizationRepository;
+    @Autowired
+    private SlideRepository slideRepository;
+
+    public ResponseEntity<Slide> deletedSlide(Long id){
+        if(slideRepository.existsById(id)){
+            Slide s = slideRepository.getById(id);
+            slideRepository.delete(s);
+            return (ResponseEntity<Slide>) ResponseEntity.ok();
+        }
+        return (ResponseEntity<Slide>) ResponseEntity.notFound();
+    }
 
 }
