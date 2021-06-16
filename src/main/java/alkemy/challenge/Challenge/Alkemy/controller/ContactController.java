1244cd5b2db70ae3911acfcb92742da6ae76d913
RolandoRapali <RolandoRapali@users.noreply.github.com>
2021-06-16 12:34:08 -0300

    Revert "Se creo el metodo necesario para traer una lista de contactos. La cual todos los contactos y se muestra un mensaje cuando no existen contactos en la base de datos"
    This reverts commit 143830aa
    
    Me equivoque y estos cambios debian ir en otra branch

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/ContactController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/ContactController.java
@@ -6,9 +6,10 @@ import alkemy.challenge.Challenge.Alkemy.util.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contacts")
@@ -30,13 +31,4 @@ public class ContactController {
         }
     }
 
-    @GetMapping("")
-    public ResponseEntity<List<Contact>> bringAllContacts() {
-        List<Contact> contacts = contactService.bringAllContacts();
-        if (contacts.isEmpty()) {
-            return new ResponseEntity(new Message("actualmente no se encuentran contactos registrados"), HttpStatus.NOT_FOUND);
-        } else {
-            return ResponseEntity.ok(contacts);
-        }
-    }
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/ContactService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/ContactService.java
@@ -5,20 +5,14 @@ import alkemy.challenge.Challenge.Alkemy.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
 
-    public void createContact(Contact contact) {
+    public void createContact(Contact contact){
         contactRepository.save(contact);
-    }
-
-    public List<Contact> bringAllContacts() {
-        return contactRepository.findAll();
     }
 
 }
