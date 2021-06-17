aa0cb172a42928bd1f5e989f8040cab1c4a38012
qwaserand <andres.emanuel.89@gmail.com>
2021-06-17 13:10:31 -0300

    Cambie el tipo de post_id a "News" y agregue id_coments

--- src/main/java/alkemy/challenge/Challenge/Alkemy/model/Comments.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/model/Comments.java
@@ -9,12 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "comments")
@@ -24,12 +22,22 @@ public class Comments implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")       
-    private Long post_id;
+    @Column(name = "id_coments")
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn
+    private News post_id;
     
     private String body;
     
     @ManyToOne
-    @JoinColumn(name = "comments_user_id")
+    @JoinColumn
     private  User user_id;
+
+    public Comments(News post_id, String body, User user_id) {
+        this.post_id = post_id;
+        this.body = body;
+        this.user_id = user_id;
+    }
 }
