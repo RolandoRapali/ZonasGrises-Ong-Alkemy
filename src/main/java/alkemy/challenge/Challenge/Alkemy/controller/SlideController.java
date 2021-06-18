f0c0f5a2b1fbc1735809851ef6f1a1a3eded4c3a
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2021-06-18 18:32:28 -0300

    datos de prueba para news y fix: NewsController @Controller -> @RestController News categoryId, quité el "lazy" en el ManyToOne, ya que daba error al deserializar la clase.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/SlideController.java
@@ -3,11 +3,10 @@ package alkemy.challenge.Challenge.Alkemy.controller;
 import alkemy.challenge.Challenge.Alkemy.model.Slide;
 import alkemy.challenge.Challenge.Alkemy.service.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -24,7 +23,7 @@ public class SlideController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Slide> updateSlide(@PathVariable(value = "id")Long id,
-            @Valid @RequestBody Slide slideDetails){
+                                             @Valid @RequestBody Slide slideDetails){
                 return slideService.updateSlide(id, slideDetails);
             }
 
