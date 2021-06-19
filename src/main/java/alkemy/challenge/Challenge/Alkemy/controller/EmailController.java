0a4d55087538c8f36f980b944cfa76a308ecd41a
German <barreragerman27@gmail.com>
2021-06-18 23:51:32 -0300

    Se creó metodo en EmailController que envía mail a un usuario confimando que se ha registrado correctamente.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/EmailController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/EmailController.java
@@ -26,20 +26,39 @@ public class EmailController {
     @PostMapping("/sendEmailTo")
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailrequest) {
         Response response = emailService.sendEmail(emailrequest);
-        if (response.getStatusCode()==200||response.getStatusCode()==202)
+        if (response.getStatusCode() == 200 || response.getStatusCode() == 202)
             return new ResponseEntity<>("Mail enviado!", HttpStatus.OK);
         return new ResponseEntity<>("Fallo el envio!", HttpStatus.NOT_FOUND);
     }
 
+    //se crea metodo para enviar mail a contacto
+    @PostMapping("/contactMail")
+    public String sendContactMail(@RequestBody EmailRequest emailRequest) {
+        Email from = new Email("yourname@yourhostname.de");
+        String subject = "Thanks for Contacting Us";
+        Email to = new Email("yourname@yourhostname.de");
+        Content content = new Content("text/html", "Dear User, your registration form has been completed successfully. We really apreciate your contact. ");
+        Mail mail = new Mail(from, subject, to, content);
 
+        SendGrid sg = new SendGrid("${sendgrid.key}");
 
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("/contactMail");
+            request.setBody(mail.build());
+
+            Response response = sg.api(request);
+
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return "email was successfully send";
+    }
 }
 
-//    Json para probar el método:
-//    
-//    {
-//    "to":"mail_cliente@mail.com",
-//    "subject":"Formulario completado / Registro exitoso, etc..",
-//    "body":"Cuerpo del mail."
-//    }
+
 
