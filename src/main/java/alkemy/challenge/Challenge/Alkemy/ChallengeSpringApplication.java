1742f45c8bc1e005c163ff1e8a9a5cb41c2ae3b6
Lucas Militello <lmilitello@gmail.com>
2021-06-22 16:39:52 -0300

    Merge origin/feature/OT39-48-creacionModificado-migración-y-modelo-de-acti
    Conflicts:
    	src/main/java/alkemy/challenge/Challenge/Alkemy/controller/ActivitiesController.java
    	src/main/java/alkemy/challenge/Challenge/Alkemy/service/ActivitiesService.java

--- src/main/java/alkemy/challenge/Challenge/Alkemy/ChallengeSpringApplication.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/ChallengeSpringApplication.java
@@ -8,6 +8,12 @@ public class ChallengeSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChallengeSpringApplication.class, args);
+        System.out.println("    ___    ____                           ______                          _____ ____ \n"
+                + "   /   |  / / /_____  ____ ___  __  __   / ____/______  ______  ____     |__  // __ \\\n"
+                + "  / /| | / / //_/ _ \\/ __ `__ \\/ / / /  / / __/ ___/ / / / __ \\/ __ \\     /_ </ /_/ /\n"
+                + " / ___ |/ / ,< /  __/ / / / / / /_/ /  / /_/ / /  / /_/ / /_/ / /_/ /   ___/ /\\__, / \n"
+                + "/_/  |_/_/_/|_|\\___/_/ /_/ /_/\\__, /   \\____/_/   \\__,_/ .___/\\____/   /____//____/  \n"
+                + "                             /____/                   /_/                            ");
     }
 
 }
