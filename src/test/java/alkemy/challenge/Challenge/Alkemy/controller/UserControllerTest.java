20e9a582d533ce0c365a70037e09ab4cdc25b7af
maximiliano <maxigiuliano18@gmail.com>
2021-06-28 19:45:01 -0300

    modificaciones a UserControllerTest

--- src/test/java/alkemy/challenge/Challenge/Alkemy/controller/UserControllerTest.java
+++ src/test/java/alkemy/challenge/Challenge/Alkemy/controller/UserControllerTest.java
@@ -1,11 +1,14 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import alkemy.challenge.Challenge.Alkemy.model.Role;
 import alkemy.challenge.Challenge.Alkemy.model.User;
 import alkemy.challenge.Challenge.Alkemy.repository.UserRepository;
 import alkemy.challenge.Challenge.Alkemy.service.UserService;
@@ -19,7 +22,11 @@ public class UserControllerTest {
 
     @BeforeEach
     void setUp() {
+        Role mockRole = new Role("ADMIN", "test role admin");
+        Optional<User> mockUser = new User();
+        new User("test", "test", "test@test.com", "12345678", "test", mockRole);
         
+        Mockito.when(userRepositoryMockito.findById((long) 1)).thenReturn(mockUser);
     }
 
     @AfterEach
