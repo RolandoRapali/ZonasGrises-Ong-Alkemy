eabefd498048808db28f8a8f4bd8c3c3e8e70bba
RolandoRapali <RolandoRapali@users.noreply.github.com>
2021-06-29 17:22:52 -0300

    Code Format

--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/ContactService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/ContactService.java
@@ -2,7 +2,6 @@ package alkemy.challenge.Challenge.Alkemy.service;
 
 import alkemy.challenge.Challenge.Alkemy.model.Contact;
 import alkemy.challenge.Challenge.Alkemy.repository.ContactRepository;
-import alkemy.challenge.Challenge.Alkemy.util.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/test/java/alkemy/challenge/Challenge/Alkemy/controller/ContactControllerTests.java
+++ src/test/java/alkemy/challenge/Challenge/Alkemy/controller/ContactControllerTests.java
@@ -19,7 +19,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -97,7 +96,7 @@ class ContactControllerTests {
         Mockito.when(contactService.bringAllContacts()).thenReturn(listContacts);
         mockMvc.perform(get("/contacts"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/deny"));/// Levantar falta de autorizacion
+                .andExpect(redirectedUrl("/deny"));
 
     }
 
@@ -162,6 +161,6 @@ class ContactControllerTests {
                 .andDo(MockMvcResultHandlers.print())
                 .andReturn();
 
-         Assert.assertEquals("", mvcResult.getResponse().getContentAsString());
+        Assert.assertEquals("", mvcResult.getResponse().getContentAsString());
     }
 }
